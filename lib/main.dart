4cde1f6b197ed0990008108d328e5dc9ae0a537e
sisouza <sidia.reis1@gmail.com>
2021-09-12 23:54:54 -0400

    keyboard, text controller

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //nossa app como um td sempre retornar um material app
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
@@ -45,11 +36,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //controller para o texto
+  var newTaskCtrl = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todo List"),
+        title: TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+          decoration: InputDecoration(
+              labelText: "Add New Iten",
+              labelStyle: TextStyle(
+                color: Colors.white,
+              )),
+        ),
         actions: <Widget>[
           Icon(Icons.plus_one),
         ],
